69900d985b0d15b43f62f03b8dc19fce2c3831c4
TheGaurdiann <rahuldadhich80263@gmail.com>
2024-11-25 16:14:20 +0530

    Course_App_Backend_Added

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+require("dotenv").config();
+console.log(process.env.MONGO_URL);
 const express = require("express");
-const app = express(); 
+const mongoose = require("mongoose");
+
+const {userRouter} = require("./Router/user");
+const {courseRouter} = require("./Router/course");
+const {adminRouter} = require("./Router/admin");
+
+const app = express() ;
+app.use(express.json());
+
+app.use("/api/v1/user" , userRouter) ;
+app.use("/api/v1/admin" , adminRouter);
+app.use("/api/v1/course", courseRouter );
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(3000);
+    console.log("listening at the port 3000");
+}
+main()
